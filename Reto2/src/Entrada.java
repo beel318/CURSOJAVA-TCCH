853e971100926ac51c7c593bbe155e9226034871
beel318 <79420402+beel318@users.noreply.github.com>
2025-04-17 14:41:27 -0600

    Add files via upload

---
+++ Reto2/src/Entrada.java
@@ -0,0 +1,5 @@
+//define de manera implicita la definicion de los atributos
+//constructor, getters, tostring
+
+public record Entrada(String nombreEvento,double precioEntrada ) {
+}
---
+++ Reto2/src/Principal.java
@@ -0,0 +1,6 @@
+public class Principal {
+    public static void main(String[] args) {
+        Entrada ev1 = new Entrada("Obra de teatro", 450.0);
+        System.out.println(ev1);
+    }
+}
